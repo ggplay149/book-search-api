880d8f1b732df118b4ad047af0e6fa76b657d4fa
syou149149 <syou149149@gmail.com>
2025-02-04 10:56:36 +0900

    add ignore

---
+++ src/main/java/com/ggplay149/book_search_service/BookSearchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ggplay149.book_search_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSearchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookSearchServiceApplication.class, args);
+	}
+
+}
